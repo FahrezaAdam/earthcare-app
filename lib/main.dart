d587116d0a00770f0903afb1fe1bc1d8ad87b644
FahrezaAdam <rezarendra29@gmail.com>
2026-06-14 16:54:00 +0700

    chore: minor format adjustments

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:google_fonts/google_fonts.dart';
 import 'src/core/router/app_router.dart';
 
